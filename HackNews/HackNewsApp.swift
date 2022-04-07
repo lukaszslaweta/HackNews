effa28b8bbf2dd42961c13ea8bd5ba8aa0f526ed
lukaszslaweta <lukasz.slaweta@gmail.com>
2022-04-07 14:15:24 +0200

    Initial Commit

---
+++ HackNews/HackNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackNewsApp.swift
+//  HackNews
+//
+//  Created by Łukasz Sławęta on 07/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct HackNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
